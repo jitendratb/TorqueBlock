a533a54e3b3831d67dca05b5183d649ec889b99c
jitendratb <jitendra@torqueblock.com>
2026-05-24 13:40:17 +0530

    feat: enable reactStrictMode, simplify image remotePatterns, and add canonical host redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'postimg.cc',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-      },
-      {
-        protocol: 'https',
-        hostname: 'postimage.me',
+        hostname: '**',
       },
     ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.torqeblock.com',
+          },
+        ],
+        destination: 'https://torqeblock.com/:path*',
+        permanent: true,
+      },
+    ];
   },
 };
 
