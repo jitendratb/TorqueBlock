3522efac4635a2cee03d9c553250d83e6e21039e
jitendratb <jitendra@torqueblock.com>
2026-09-15 15:22:23 +0530

    style: change checkout vertical padding to bottom and remove commented test api url

--- src/app/(shop)/Components/Checkout/CheckoutClient.js
+++ src/app/(shop)/Components/Checkout/CheckoutClient.js
@@ -391,7 +391,7 @@ export default function CheckoutClient() {
 
     return (
         <>
-            <div className="relative z-10 space-y-4 py-4">
+            <div className="relative z-10 space-y-4 pb-4">
                 <div className="p-4 rounded-xl bg-white/10 border border-white/10 backdrop-blur-2xl shadow-[0_10px_40px_rgba(0,0,0,0.3)] relative overflow-hidden">
 
                     <div className="flex flex-col lg:flex-row lg:items-center justify-between gap-6">
