173727608d2293b890ea8b6880046d3f47579a0f
jitendratb <jitendra@torqueblock.com>
2026-09-26 19:12:47 +0530

    drftgyh

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -30,7 +30,7 @@ import FAQSchema from '@/components/seo/FAQSchema'
 import LocalBusinessSchema from '@/components/seo/LocalBusinessSchema'
 import { getTrustindexStats } from '@/services/trustindexStats'
 
-export const revalidate = 3600;
+export const revalidate = 0;
 
 
 const POPULAR_SEARCHES = [
